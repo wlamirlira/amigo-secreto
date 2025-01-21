23a8170c321cb643cab46ed09a0debb389d57208
Wlamir Lira <wlamirl@gmail.com>
2025-01-21 12:14:40 -0300

    primeira entrega -> função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    
+    let nome = document.querySelector('input').value
+    if(nome == ''){
+        alert('Insira um nome!');
+    }else{
+        amigos.push(nome);
+        console.log(amigos);
+        limparCampo();
+    }
+    exibirLista()
+}
+function exibirLista(){
+    for (let i = 0; i < amigos.length; i++) {
+        console.log(amigos[i])
+    }
+}
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+
